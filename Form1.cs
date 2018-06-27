4bdce5b5c2e1037a2b21be64f8d3491b04095f47
Leigh Jones <40029484+leighmjones@users.noreply.github.com>
2018-06-27 18:13:33 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LightHueForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mixButton_Click(object sender, EventArgs e)
+        {
+                LightColors FirstLight = new LightColors();
+                LightColors SecondLight = new LightColors();
+                LightColors Result = new LightColors();
+                //LightColors FinalColor = new LightColors();
+           
+                FirstLight.Hue = textBox1.Text;
+                SecondLight.Hue = textBox2.Text;
+
+                Result = FirstLight + SecondLight;
+
+            switch (Result.Hue)
+            {
+                case "YELLOW":
+                    colorBox.BackColor = Color.Yellow;
+                    colorBox.ForeColor = Color.Yellow; break;
+                case "RED":
+                    colorBox.BackColor = Color.Red;
+                    colorBox.ForeColor = Color.Red; break;
+                case "MAGENTA":
+                    colorBox.BackColor = Color.Magenta;
+                    colorBox.ForeColor = Color.Magenta; break;
+                case "VIOLET":
+                    colorBox.BackColor = Color.Violet;
+                    colorBox.ForeColor = Color.Violet; break;
+                case "ROSE":
+                    colorBox.BackColor = Color.MistyRose;
+                    colorBox.ForeColor = Color.MistyRose; break;
+                case "GREEN":
+                    colorBox.BackColor = Color.ForestGreen;
+                    colorBox.ForeColor = Color.ForestGreen; break;
+                case "CYAN":
+                    colorBox.BackColor = Color.Cyan;
+                    colorBox.ForeColor = Color.Cyan; break;
+                case "BLUE":
+                    colorBox.BackColor = Color.Blue;
+                    colorBox.ForeColor = Color.Blue; break;
+                case "AZURE":
+                    colorBox.BackColor = Color.Azure;
+                    colorBox.ForeColor = Color.Azure; break;
+                case "SPRING GREEN":
+                    colorBox.BackColor = Color.SpringGreen;
+                    colorBox.ForeColor = Color.SpringGreen; break;
+                case "CHARTREUSE GREEN":
+                    colorBox.BackColor = Color.Chartreuse;
+                    colorBox.ForeColor = Color.Chartreuse; break;
+
+
+            }
+            
+        }
+    }
+}
---
+++ LightColors.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightHueForm
+{
+    class LightColors
+    {
+
+        public string Hue { get; set; }
+
+        //Constructor
+        public  LightColors()
+        {
+            Hue = "WHITE";
+        }
+
+        //Define action for the + operator
+        public static LightColors operator +(LightColors L1, LightColors L2)
+        {
+            LightColors MixedColor = new LightColors();
+            string Light1 = L1.Hue.ToUpper();
+            string Light2 = L2.Hue.ToUpper();
+
+            #region RED
+            if (Light1 == "RED")
+            {
+                if (Light2 == "RED")
+                {
+                    MixedColor.Hue = "RED";
+                }
+                else if (Light2 == "GREEN")
+                {
+                    MixedColor.Hue = "YELLOW";
+                }
+                else if (Light2 == "BLUE")
+                {
+                    MixedColor.Hue = "MAGENTA";
+                }
+                else if (Light2 == "MAGENTA")
+                {
+                    MixedColor.Hue = "ROSE";
+                }
+                else if (Light2 == "YELLOW")
+                {
+                    MixedColor.Hue = "ORANGE";
+                }
+                else
+                {
+                    MixedColor.Hue = Light1 + "-" + Light2;
+                }
+            }//end if 
+            #endregion
+
+            #region GREEN
+            else if (Light1 == "GREEN")
+            {
+                if (Light2 == "GREEN")
+                {
+                    MixedColor.Hue = "GREEN";
+                }
+                else if (Light2 == "BLUE")
+                {
+                    MixedColor.Hue = "CYAN";
+                }
+                else if (Light2 == "RED")
+                {
+                    MixedColor.Hue = "MAGENTA";
+                }
+                else if (Light2 == "CYAN")
+                {
+                    MixedColor.Hue = "SPRING GREEN";
+                }
+                else if (Light2 == "YELLOW")
+                {
+                    MixedColor.Hue = "CHARTREUSE GREEN";
+                }
+                else
+                {
+                    MixedColor.Hue = Light1 + "-" + Light2;
+                }
+            }//end iff
+            #endregion
+
+            #region BLUE
+            else if (Light1 == "BLUE")
+            {
+                if (Light2 == "BLUE")
+                {
+                    MixedColor.Hue = "BLUE";
+                }
+                else if (Light2 == "RED")
+                {
+                    MixedColor.Hue = "MAGENTA";
+                }
+                else if (Light2 == "GREEN")
+                {
+                    MixedColor.Hue = "CYAN";
+                }
+                else if (Light2 == "CYAN")
+                {
+                    MixedColor.Hue = "AZURE";
+                }
+                else if (Light2 == "MAGENTA")
+                {
+                    MixedColor.Hue = "VIOLET";
+                }
+                else
+                {
+                    MixedColor.Hue = Light1 + "-" + Light2;
+                }
+            }//end if
+            #endregion
+
+            #region MAGENTA
+            if (Light1 == "MAGENTA")
+            {
+                 if (Light2 == "BLUE")
+                 {
+                     MixedColor.Hue = "VIOLET";
+                 }
+                 else if (Light2 == "RED")
+                 {
+                     MixedColor.Hue = "ROSE";
+                 }
+                 else
+                 {
+                     MixedColor.Hue = Light1 + "-" + Light2;
+                 }
+             }
+            #endregion
+
+            #region CYAN
+            if (Light1 == "CYAN")
+            {
+                if (Light2 == "BLUE")
+                {
+                    MixedColor.Hue = "AZURE";
+                }
+                else if (Light2 == "GREEN")
+                {
+                    MixedColor.Hue = "SPRING GREEN";
+                }
+            }
+            #endregion
+
+            #region YELLOW
+            if (Light1 == "YELLOW")
+            {
+                if (Light2 == "GREEN")
+                {
+                    MixedColor.Hue = "CHARTREUSE";
+                }
+                else if (Light2 == "RED")
+                {
+                    MixedColor.Hue = "ORANGE";
+                }
+            }
+            #endregion      
+
+            return MixedColor;
+        }
+    }
+}
